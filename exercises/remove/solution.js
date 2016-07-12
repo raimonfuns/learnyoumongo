1c113edd7c06567f5f47dd67ee8a815cba16bdbf
raimonfuns <raimonfuns@163.com>
2016-07-12 13:47:00 +0800

    remove

---
+++ exercises/remove/solution.js
@@ -0,0 +1,17 @@
+var mongo = require('mongodb').MongoClient
+
+var dbName = process.argv[2]
+var collectionName = process.argv[3]
+var id = process.argv[4]
+
+var url = 'mongodb://localhost:27017/' + dbName
+mongo.connect(url, function (err, db) {
+  if (err) throw err
+  var collection = db.collection(collectionName)
+  collection.remove({
+    _id: id
+  }, function (err) {
+    if (err) throw err
+    db.close()
+  })
+})
